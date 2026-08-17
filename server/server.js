8b7b2031288277512275b0779b63e4707fc0cfe6
shalini-dev <shaliniprajapati2012@gmail.com>
2026-08-17 23:32:12 +0530

    Build contact API with MongoDB integration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import contactRoutes from "./routes/contact.route.js";
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contact", contactRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Portfolio API is running 🚀" });
