dc4e075a5183a7647f367d662c9abdc92b485f97
shalini-dev <shaliniprajapati2012@gmail.com>
2026-08-17 23:12:00 +0530

    Setup Express server with MongoDB Atlas connection

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Portfolio API is running 🚀" });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
